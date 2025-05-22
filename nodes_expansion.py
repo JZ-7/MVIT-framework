6b7092d91a3260d026dbd24786a9f91a82582e21
JZ-7 <jzmath@buaa.edu.cn>
2025-05-22 20:04:15 +0800

    nodes_expansion.py

--- nodes_expansion.py
+++ nodes_expansion.py
@@ -1,7 +1,6 @@
 import numpy as np
 from reservoirpy import Node
 
-#square_node:返回由输入向量和其（各分量分别）平方得到的向量连接组成的向量
 def square_forward(node: Node, x: np.ndarray) -> np.ndarray:
     return np.c_[x, np.square(x)]
 def initialize(node: Node, x: np.ndarray = None, y: np.ndarray = None):
@@ -22,12 +21,7 @@ class Square_node(Node):
             params={"const1": None},
             name=name
         )
-#square_node = CustomNode(name="square_node")
-# x = np.array([1, 2, 3]).reshape(1, -1)
-# y = square_node(x)
-# print(y)
 
-#solesquare_node:返回输入向量各分量分别平方后得到的向量
 def solesquare_forward(node: Node, x: np.ndarray) -> np.ndarray:
     return np.square(x)
 def solesquare_initialize(node: Node, x: np.ndarray = None, y: np.ndarray = None):
@@ -41,8 +35,4 @@ class SoleSquare_node(Node):
             initializer=solesquare_initialize,
             name=name
         )
-# solesquare_node = SoleSquare_node(name="solesquare_node")
-# x = np.array([1, 2, 3]).reshape(1, -1)
-# y = solesquare_node(x)
-# print(y)
 
